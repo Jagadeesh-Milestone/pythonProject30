c7c7f73f6b6e8dca8406d18b13fc942b5b9bf238
jagadeesh-python <87078028+jagadeesh-python@users.noreply.github.com>
2024-01-29 11:14:30 +0530

    atm usecase

--- atm.py
+++ atm.py
@@ -14,4 +14,47 @@
 #1--check balance
 #2--deposit amount--amount>0,balance+deposit amount
 #3--withdraw amount--amount>0,withdraw amount<balance,balance-withdraw amount
-
+import time
+t=time.ctime()
+print(t)
+print('\t\t\t welcome to ICICI Bank ATM')
+name=input('please enter your name:')
+print('hi',name)
+while True:
+    print('please select your language:')
+    print('1.telugu\n2.engilsh\n3.hindi')
+    language = int(input('enter your language:'))
+    if language not in range(1,4):
+        print('enter valid option')
+        break
+    else:
+        pass
+        balance=50000
+        pin=input('please enter your 4 digits pin:')
+        if pin.isdigit() and len(pin)==4:
+            print('select the following options')
+            print('1.account balance\n2.deposit amount\n3.withdraw amount')
+            option=int(input('enter your option:'))
+            if option==1:
+                print('your account balance is:',balance)
+            elif option==2:
+                deposit=int(input('enter the deposit amount:'))
+                if deposit>0:
+                    print('the amount has been deposited successfully')
+                    print('updated account balance is:',deposit+balance)
+                else:
+                    print('enter the amount greater than 0')
+                break
+            elif option==3:
+                 withdraw=int(input('enter the amount to withdraw:'))
+                 if withdraw>0 and withdraw<balance:
+                   print('your transaction of',withdraw,'has been successfull')
+                   print('the updated balance is:',balance-withdraw)
+                 else:
+                   print('enter the amount greater than 0 or amount less than balance')
+                 break
+            else:
+             print('enter the valid option')
+             break
+        else:
+            print('enter valid pin')
